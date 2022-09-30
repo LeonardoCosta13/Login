a7d2b0dbaa77b52aa8735943c7f68f9905e6b589
leonardocosta <leocostasousa123@gmail.com>
2022-09-30 16:28:11 -0300

    first commit

---
+++ curso-de-angular-auth-front/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminModule } from './components/admin/admin.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./core/components/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./components/admin/admin.module').then(m=>AdminModule),
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
